402a33c493103831d4d0fcd477ab2975989ffe83
hujduralen <alen@sigura.eu>
2018-10-29 23:35:20 +0100

    templates

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,12 +2,11 @@ import DS from 'ember-data'
 
 export default DS.RESTAdapter.extend({
     urlForFindAll(modelName, snapshot){
-        // if(modelName == 'shop')
-        //     return 'shops.json';
-        // else if(modeName == 'etison')
-        //     return 'etisons.json';
-        // else
-        //     return;
-        return 'etisons.json';
+        if(modelName == 'shop')
+            return 'shops.json';
+        else if(modeName == 'etison')
+            return 'etisons.json';
+        else
+            return;
     }
 });
