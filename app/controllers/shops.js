e649398ca20e6004d82de5673f25673b9496af32
hujduralen <alen@sigura.eu>
2018-11-04 19:54:01 +0100

    setting up controller actions

---
+++ app/controllers/shops.js
@@ -0,0 +1,12 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+// actions: {
+//     thingsTolog(){
+//         //console.log("ok -test 123");
+//         alert("Alarm alarm");
+//     }
+// }
+
+});
